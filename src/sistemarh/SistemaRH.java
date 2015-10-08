da66e780f0e86e8a0c14523fb921e613eee967fd
Samara Borges <samy.tiemi@gmail.com>
2015-10-08 14:12:03 -0300

    Criação de projeto

---
+++ src/sistemarh/SistemaRH.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemarh;
+
+/**
+ *
+ * @author Samara
+ */
+public class SistemaRH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
